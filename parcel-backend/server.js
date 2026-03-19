677d3c750ec21e436836e1e35b7c6e7219a07d7c
Shivani Jadhav <157908746+Shivpjadhav@users.noreply.github.com>
2026-03-19 21:08:59 +0530

    Update server.js

--- parcel-backend/server.js
+++ parcel-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
   
 const app = express(); 
-app.use(express.json());
+app.use(express.json()); 
  
 // CONNECT DATABASE
 mongoose.connect('YOUR_MONGODB_CONNECTION_STRING')
